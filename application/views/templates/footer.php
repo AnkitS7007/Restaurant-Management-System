e364021fb0d28cb83758bf05d0010d178bf3d513
Ankit Sharma <ankitbond7007@gmail.com>
2023-10-31 16:11:58 +0530

    comit

---
+++ application/views/templates/footer.php
@@ -0,0 +1,14 @@
+<footer class="main-footer">
+  <strong>&copy;
+    <?php echo date('Y'); ?> |
+  </strong> All rights
+  reserved - Restaurant Management System by <b>Ankit Sharma </b>
+</footer>
+
+<div class="control-sidebar-bg"></div>
+</div>
+
+
+</body>
+
+</html>
---
+++ application/views/templates/header_menu.php
@@ -0,0 +1,12 @@
+<header class="main-header">
+  <div href="" class="logo">
+    <span class="logo-mini"><b>RD</b></span>
+    <span class="logo-lg"><b>Restro Dine</b></span>
+  </div>
+  <nav class="navbar navbar-static-top">
+    <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+      <span class="sr-only">Toggle navigation</span>
+    </a>
+
+  </nav>
+</header>
